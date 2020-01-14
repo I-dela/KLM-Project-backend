1eb33e118e7185efccdc51d5c86b937565b383ed
idodela <i_delawar@hotmail.com>
2020-01-15 00:11:08 +0100

    Ilias Tests

--- src/test/java/Klm1/KLMLineMaintenanceServer/Ilias.java
+++ src/test/java/Klm1/KLMLineMaintenanceServer/Ilias.java
@@ -6,30 +6,29 @@ Made By : Ilias Delawar
 
 import Klm1.KLMLineMaintenanceServer.controllers.*;
 import Klm1.KLMLineMaintenanceServer.models.*;
+import Klm1.KLMLineMaintenanceServer.models.helper.UserNotFoundException;
 import Klm1.KLMLineMaintenanceServer.repositories.*;
 import Klm1.KLMLineMaintenanceServer.repositories.interfaces.UserRepository;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 //is used to provide a bridge between Spring Boot test features and JUnit.Whenever we are using any Spring Boot testing features in our JUnit tests, this annotation will be required.
-@RunWith(SpringRunner.class)
 // specify the main applcication class(optional)
 @SpringBootTest(classes = KlmLineMaintenanceServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TestIlias {
@@ -87,9 +86,9 @@ class TestIlias {
         //check if save is successful by checking attributes
         System.out.println(savedEquipment.getSerialNumber());
 
-        Assert.assertEquals("TestSerial", savedEquipment.getSerialNumber());
+        assertEquals("TestSerial", savedEquipment.getSerialNumber());
 
-        Assert.assertEquals("TestId", savedEquipment.getId());
+        assertEquals("TestId", savedEquipment.getId());
 
 
     }
@@ -110,7 +109,7 @@ class TestIlias {
 
         // find previously deleted entry and check if the method return null. (it should return null because the entry should be deleted in the database)
         Equipment equipmentDeleted = equipmentRepositoryJpa.findBySerialNumber("TestSerial");
-        Assert.assertNull(equipmentDeleted);
+        assertNull(equipmentDeleted);
     }
 
 
@@ -131,7 +130,8 @@ class TestIlias {
         // CHECK IF THE EQUIPMENT FOUND HAS THE STATUS
 
         // check if the status of the equipment is actually changed
-        Assert.assertEquals(Equipment.Status.Usable, equipment.getStatus());
+        assertEquals(Equipment.Status.Usable, equipment.getStatus());
+
 
         //change it back to its original value
         equipmentRepositoryJpa.setEquipmentStatus(equipment.getSerialNumber(), Equipment.Status.Inuse);
@@ -162,13 +162,13 @@ class TestIlias {
         this.requestRepositoryJpa.setRequestStatus(requestGet.getId(), Request.Status.IP);
 
         // check wheter the request in the database is changed to (In progress )
-        Assert.assertThat(this.requestRepositoryJpa.findById(requestGet.getId()).getStatus(), is(Request.Status.IP));
+        assertThat(this.requestRepositoryJpa.findById(requestGet.getId()).getStatus(), is(Request.Status.IP));
 
         // cancel the request(it should then return to open(OP))
         this.requestRepositoryJpa.cancelRequestRun(requestGet.getId());
 
         // check whether the status is changed back to open OP
-        Assert.assertThat(this.requestRepositoryJpa.findById(requestGet.getId()).getStatus(), is(Request.Status.OP));
+        assertThat(this.requestRepositoryJpa.findById(requestGet.getId()).getStatus(), is(Request.Status.OP));
 
     }
 
@@ -184,17 +184,17 @@ class TestIlias {
         ResponseEntity equipmentResponseEntity = this.equipmentControllerTest.postEquipment(equipment);
 
         // check if the equipment is created by checking the statusCode
-        Assert.assertEquals(HttpStatus.CREATED, equipmentResponseEntity.getStatusCode());
+        assertEquals(HttpStatus.CREATED, equipmentResponseEntity.getStatusCode());
 
         // retrieve the saved equipment
         Equipment savedEquipment = this.equipmentControllerTest.getEquipment(equipment.getSerialNumber());
 
         // Check if the equipment that was saved is the same as the equipment object we made in the beginning
-        Assert.assertSame(equipment.getSerialNumber(), savedEquipment.getSerialNumber());
+        assertSame(equipment.getSerialNumber(), savedEquipment.getSerialNumber());
 
         this.equipmentControllerTest.deleteEquipment(equipment.getSerialNumber());
 
-        Assert.assertNull(this.equipmentControllerTest.getEquipment(equipment.getSerialNumber()));
+        assertNull(this.equipmentControllerTest.getEquipment(equipment.getSerialNumber()));
 
     }
 
@@ -225,7 +225,7 @@ class TestIlias {
 
         // check whether the first element in the list is actually the Request that needs to be handled first because of the departure time
         assert priorityRequest != null;
-        Assert.assertEquals(orderdRequests.get(0).getDeparture(), priorityRequest.getDeparture());
+        assertEquals(orderdRequests.get(0).getDeparture(), priorityRequest.getDeparture());
     }
 
 
@@ -246,7 +246,7 @@ class TestIlias {
         String message = locationController.updateLocation(locationUpdate);
 
         // if the string contains the "updates successfully message" the update is successfull
-        Assert.assertEquals("updated successfully", message);
+        assertEquals("updated successfully", message);
 
         /*  check post method of the locationController */
 
@@ -257,41 +257,33 @@ class TestIlias {
         this.locationController.postLocation(new Location(new ArrayList<>(), "A50", Location.Type.Pier));
 
         // check if the list that getLocation() gives back has increased after adding a location
-        Assert.assertTrue(this.locationController.getLocation().size() > initialLocationsSize);
+        assertTrue(this.locationController.getLocation().size() > initialLocationsSize);
 
         /* Finally test if the delete method works in the locationController */
         this.locationController.deleteLocation("A50");
 
         // find previously deleted entry and check if the method return null. (it should return null because the entry should be deleted in the database)
-        Assert.assertNull(this.locationController.getLocationById("A50"));
+        assertNull(this.locationController.getLocationById("A50"));
     }
 
 
     @Test
-    public void user_tests() {
-        /* this method tests te methods of users repo and controller */
+    public void user_tests() throws UserNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 
-        // make object of user
-        User user = new User();
+            assertThrows(NoSuchMethodException.class, () -> {
 
-        // set attributes
-        user.setName("Test");
-        user.setRole("RUN");
-        user.setPassword("12345");
-        user.setStatus("OFF");
+                Class userClass = User.class;
 
-        // post the user
-        String message = this.userController.postUsers(user);
+                Constructor userCon = userClass.getConstructor(new Class[]{int.class});
 
-        // check if the user is saved successfully
-        Assert.assertEquals("User saved succesfully", message);
+                User user  = (User) userCon.newInstance(1);
+            });
 
-        // get the posted user from the database
-        User getPostedUser = this.userController.getUserById(user.getId());
+        }
 
-        // check if users are the same by  comparing their name
-        Assert.assertEquals("Test", getPostedUser.getName());
-    }
+
+
+
 
 
     @Test
@@ -309,7 +301,7 @@ class TestIlias {
       ResponseEntity responseEntity =  aircraftController.postAircraft(aircraft);
 
       // check if the statuscode that is given back in the responseidentity is indeed the correct one
-        Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
 
         // get the aircraft that wasd posted
         aircraft = aircraftController.getAircraft(aircraft.getId());
@@ -321,12 +313,10 @@ class TestIlias {
         this.aircraftRepositoryJpa.delete(aircraft);
 
         // check if the aircraft that is deleted is requested again that it gives null as an result
-        Assert.assertNull(this.aircraftController.getAircraft(aircraft.getId()));
+        assertNull(this.aircraftController.getAircraft(aircraft.getId()));
     }
 }
 
-
-@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest(classes = KlmLineMaintenanceServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TestMockIlias{
 
